e14e14504d80539d130eb02849933da13273c219
Tom Fyfe <tomfyfe85@gmail.com>
2023-05-25 14:02:01 +0100

    Project Setup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp.Intro
+{
+  class Calculator
+  {
+    static void Main(string[] args)
+    {
+      {
+        // note that the we have to declare a type (int) for the variable (result)
+        int result = 2 + 2;
+        Console.WriteLine("The result is {0:d}", result);
+      }
+    }
+  }
+}
